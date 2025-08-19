c2bd2cd887fbeaa340d4df1a49850a84967cb42a
psychic1999 <102906402+psychic1999@users.noreply.github.com>
2025-08-19 20:29:26 +0800

    Update YouTube-is-cancer.user.js

--- YouTube-is-cancer.user.js
+++ YouTube-is-cancer.user.js
@@ -3,7 +3,7 @@
 // @namespace   youtube
 // @match       *://*.youtube.com/*
 // @grant       none
-// @version     2.18.7
+// @version     2.18.8
 // @author      psychic1999
 // @description YouTube is cancer
 // @downloadURL https://raw.githubusercontent.com/psychic1999/YouTube-is-cancer/refs/heads/main/YouTube-is-cancer.user.js
@@ -38,7 +38,7 @@ ytm-channel-list-sub-menu-renderer{
 /*SCREEN DESKTOP*/
 html[dark]{
 ytd-masthead,
-#background.ytd-masthead,
+ytd-masthead[frosted-glass-mode=without-chipbar] #background.ytd-masthead,
 #contentContainer.tp-yt-app-drawer,
 #guide-content.ytd-app,
 ytd-mini-guide-renderer,
